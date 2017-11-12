a992fa8dc8102245a4126b346896526fb4ef2e03
wesam mustafa <wesam.mustafa100@gmail.com>
2017-11-12 04:41:07 +0200

    Using preloading Strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from "@angular/core";
-import {Routes, RouterModule} from "@angular/router";
+import {Routes, RouterModule,PreloadAllModules} from "@angular/router";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { HomeComponent } from "./core/home/home.component";
 
@@ -10,7 +10,9 @@ const appRouts: Routes = [
     {path:'shopping-list',component:ShoppingListComponent}
 ];
 @NgModule({   
-imports:[RouterModule.forRoot(appRouts)],
+imports:[
+    RouterModule.forRoot(appRouts,{preloadingStrategy:PreloadAllModules})
+],
 exports:[RouterModule]
 })
 export class AppRoutingModule {
